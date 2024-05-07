d3dbade696d90153c5333995fbb882fd04f306e8
Amir Raminfar <findamir@gmail.com>
2024-05-07 08:30:35 -0700

    updates FDM

--- env.fish
+++ env.fish
@@ -1,6 +1,8 @@
 #!/usr/bin/env fish
 
 set -Ux RIPGREP_CONFIG_PATH $HOME/.ripgreprc
+set -Ux FNM_COREPACK_ENABLED true 
+set -Ux COREPACK_ENABLE_DOWNLOAD_PROMPT 0
 
 abbr -a -U -- - 'cd -'
 abbr -a -U -- l ls
