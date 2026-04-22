2675b0b1789c8cea7db79ab26dca6a849dec9b3f
Amir Raminfar <findamir@gmail.com>
2026-04-22 07:07:02 -0700

    clean up

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+FISH_PATH="/opt/homebrew/bin/fish"
+
 # Install brew if it doesn't already exist
-command -v brew > /dev/null 2>&1  || (yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
+command -v brew > /dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install all things brew related
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
@@ -11,22 +13,25 @@ brew bundle
 
 # Setup fish
 echo "Setting up fish..."
-grep -q -F '/usr/local/bin/fish' /etc/shells || echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
-test $SHELL = "/usr/local/bin/fish" || chsh -s /usr/local/bin/fish
+grep -q -F "$FISH_PATH" /etc/shells || echo "$FISH_PATH" | sudo tee -a /etc/shells
+test "$SHELL" = "$FISH_PATH" || chsh -s "$FISH_PATH"
 
 echo "Setting up fisher..."
 # Install fisher
 fish -c 'fisher' || fish -c 'curl -sL git.io/fisher | source && fisher install jorgebucaran/fisher'
 
-# Stow fisher
-stow fisher lsd git starship pep ssh vim fish rg
+# Stow configs
+stow fisher lsd git starship pep ssh vim fish rg tmux ghostty claude
 
 # Run fisher
 fish -c 'fisher' || fish -c 'curl -sL git.io/fisher | source && fisher update'
 
 fish ./env.fish
 
-# Install vim-sensible
-mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/junegunn/fzf.vim.git ~/.vim/pack/packages/start/fzf.vim
-(cd ~/.vim/bundle && git clone https://github.com/tpope/vim-sensible.git &&  git clone https://github.com/itchyny/lightline.vim)
+# Install vim plugins via native packages
+VIM_PACK=~/.vim/pack/packages/start
+mkdir -p "$VIM_PACK"
+for repo in tpope/vim-sensible itchyny/lightline.vim junegunn/fzf.vim; do
+    name="${repo##*/}"
+    test -d "$VIM_PACK/$name" || git clone "https://github.com/$repo.git" "$VIM_PACK/$name"
+done
